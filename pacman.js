21414686adca7c63589c467a33b34e01370696cd
“Willie” <willie.williams@codeimmersives.com>
2021-05-02 07:58:40 -0400

    characters

--- pacman.js
+++ pacman.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const scoreDisplay = document.getElementById('score')
     const width = 28
     let score = 0
@@ -36,84 +35,24 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
 const squares = []
+
 function createBoard() {
     for (let i = 0; i < layout.length; i++) {
-    const square = document.createElement('div')
-    grid.appendChild(square)
-    squares.push(square)
-
-    if(layout[i] === 0) {
+        const square = document.createElement('div')
+        grid.appendChild(square)
+        squares.push(square)
+        if(layout[i] === 0) {
         squares[i].classList.add('pac-dot')
-    } else if (layout[i] === 1) {
+        } else if (layout[i] === 1) {
         squares[i].classList.add('wall')
-    } else if (layout[i] === 2) {
+        } else if (layout[i] === 2) {
         squares[i].classList.add('ghost-lair')
-    } else if (layout[i] === 3) {
+        } else if (layout[i] === 3) {
         squares[i].classList.add('power-pellet')
+        }
+    
     }
-    }
-}
 
-createBoard()
-
-
-//create Characters
-//draw pacman onto the board
-let pacmanCurrentIndex = 490
-squares[pacmanCurrentIndex].classList.add('pac-man')
-//get the coordinates of pacman on the grid with X and Y axis
-// function getCoordinates(index) {
-//   return [index % width, Math.floor(index / width)]
-// }
-
-// console.log(getCoordinates(pacmanCurrentIndex))
-
-//move pacman
-function movePacman(e) {
-  squares[pacmanCurrentIndex].classList.remove('pac-man')
-  switch(e.keyCode) {
-    case 37:
-      if(
-        pacmanCurrentIndex % width !== 0 &&
-        !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
-        !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')
-        )
-      pacmanCurrentIndex -= 1
-      if (squares[pacmanCurrentIndex -1] === squares[363]) {
-        pacmanCurrentIndex = 391
-      }
-      break
-    case 38:
-      if(
-        pacmanCurrentIndex - width >= 0 &&
-        !squares[pacmanCurrentIndex -width].classList.contains('wall') &&
-        !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')
-        ) 
-      pacmanCurrentIndex -= width
-      break
-    case 39:
-      if(
-        pacmanCurrentIndex % width < width - 1 &&
-        !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
-        !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')
-      )
-      pacmanCurrentIndex += 1
-      if (squares[pacmanCurrentIndex +1] === squares[392]) {
-        pacmanCurrentIndex = 364
-      }
-      break
-    case 40:
-      if (
-        pacmanCurrentIndex + width < width * width &&
-        !squares[pacmanCurrentIndex +width].classList.contains('wall') &&
-        !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')
-      )
-      pacmanCurrentIndex += width
-      break
-  }
-  squares[pacmanCurrentIndex].classList.add('pac-man')
-  pacDotEaten()
-  powerPelletEaten()
-  checkForGameOver()
-  checkForWin()
-}
+    createBoard()
+    let pacmanCurrentIndex = 490
+    squares[pacmanCurrentIndex].classList.add('pac-man')
